ef585a815ac4d540ddab8c878933f54dde454c38
zoldykzino5-cell <zoldykzino5@gmail.com>
2025-09-13 21:42:26 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~o7tliBDI#hAnwqjmDM-F-L71GE7gcgSnBdgqlmgJX0WnYuPc6Bpw",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+23569750414", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
